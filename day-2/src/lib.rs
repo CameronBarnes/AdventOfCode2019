6a8a3c9ab748688450fba085aa970d30c2e7fb30
Cameron Barnes <cameron_barnes@outlook.com>
2024-11-21 23:14:05 -0800

    refactored day 2 and completed part 2

--- day-2/src/lib.rs
+++ day-2/src/lib.rs
@@ -1,2 +1,35 @@
 pub mod part1;
 pub mod part2;
+
+pub fn parse_program(input: &str) -> Vec<usize> {
+    input
+        .lines()
+        .flat_map(|line| line.split(','))
+        .map(|num| num.parse::<usize>().unwrap())
+        .collect()
+}
+
+pub fn run_program(program: &mut [usize]) {
+    let mut index = 0;
+    while index + 3 < program.len() {
+        let operand = *program.get(index).unwrap();
+        let pos1 = *program.get(index + 1).unwrap();
+        let pos2 = *program.get(index + 2).unwrap();
+        let dest = *program.get(index + 3).unwrap();
+        let value1 = *program.get(pos1).unwrap();
+        let value2 = *program.get(pos2).unwrap();
+        match operand {
+            1 => {
+                *program.get_mut(dest).unwrap() = value1 + value2;
+            }
+            2 => {
+                *program.get_mut(dest).unwrap() = value1 * value2;
+            }
+            99 => {
+                break;
+            }
+            _ => panic!("Invalid operand"),
+        }
+        index += 4;
+    }
+}
--- day-2/src/part2.rs
+++ day-2/src/part2.rs
@@ -1,7 +1,21 @@
+fn run_attempt(mut program: Vec<usize>, input1: usize, input2: usize) -> usize {
+    program[1] = input1;
+    program[2] = input2;
+    crate::run_program(&mut program);
+    *program.first().unwrap()
+}
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> String {
-    todo!("day-2 - part 2")
+pub fn process(input: &str) -> String {
+    let program = crate::parse_program(input);
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            if run_attempt(program.clone(), noun, verb) == 19690720 {
+                return (100 * noun + verb).to_string()
+            }
+        }
+    }
+    "failed".to_string()
 }
 
 #[cfg(test)]
@@ -10,8 +24,7 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = include_str!("../input.txt");
+        assert_eq!("7960", process(input));
     }
 }
