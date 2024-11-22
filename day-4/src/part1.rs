3e26638596853af196e88a943510eb79601f2f55
Cameron Barnes <cameron_barnes@outlook.com>
2024-11-22 14:22:06 -0800

    completed day 4, part 1

---
+++ day-4/src/part1.rs
@@ -0,0 +1,46 @@
+use itertools::Itertools;
+use rayon::iter::{ParallelBridge, ParallelIterator};
+
+fn test_password(password: &usize) -> bool {
+    let password = password.to_string().chars().collect_vec();
+    password.len() == 6
+        && password
+            .windows(2)
+            .any(|window| window[0] == window[1])
+        && password
+            .windows(2)
+            .all(|window| window[0] <= window[1])
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> String {
+    let (lower_bound, higher_bound) = input.trim().split_once('-').unwrap();
+    let (lower_bound, higher_bound) = (
+        lower_bound.parse::<usize>().unwrap(),
+        higher_bound.parse::<usize>().unwrap(),
+    );
+
+    (lower_bound..=higher_bound)
+        .par_bridge()
+        .filter(test_password)
+        .count()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case(132279, false)]
+    #[case(133679, true)]
+    #[case(111111, true)]
+    #[case(223450, false)]
+    #[case(123789, false)]
+    fn test_password_check(#[case] input: usize, #[case] valid: bool) {
+        println!("Password: {input}, should be valid: {valid}");
+        assert_eq!(valid, test_password(&input));
+    }
+}
