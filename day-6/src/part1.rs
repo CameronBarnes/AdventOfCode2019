00382c0dde5841b47fafff92113c4f8d4b9c1788
Cameron Barnes <cameron_barnes@outlook.com>
2024-11-25 14:33:07 -0800

    completed day 6, part 1

---
+++ day-6/src/part1.rs
@@ -0,0 +1,63 @@
+use std::{cell::RefCell, rc::Rc};
+
+use ahash::{HashMap, HashMapExt};
+
+#[derive(Default)]
+struct Object {
+    children: Vec<Rc<RefCell<Object>>>,
+}
+
+impl Object {
+    fn new_with_child(child: Rc<RefCell<Object>>) -> Self {
+        Self {
+            children: vec![child],
+        }
+    }
+}
+
+fn count_object(mut depth: usize, object: Rc<RefCell<Object>>) -> usize {
+    depth += 1;
+    let mut count = 0;
+    for child in object.borrow().children.clone() {
+        count += depth;
+        count += count_object(depth, child);
+    }
+    count
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> String {
+    let mut map: HashMap<String, Rc<RefCell<Object>>> = HashMap::new();
+    input
+        .lines()
+        .map(|line| line.trim().split_once(')').unwrap())
+        .for_each(|(parent, child)| {
+            let child = map.entry(child.to_string()).or_default().clone();
+            map.entry(parent.to_string())
+                .and_modify(|object| object.borrow_mut().children.push(child.clone()))
+                .or_insert(Rc::new(RefCell::new(Object::new_with_child(child))));
+        });
+
+    count_object(0, map.get("COM").unwrap().clone()).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L";
+        assert_eq!("42", process(input));
+    }
+}
