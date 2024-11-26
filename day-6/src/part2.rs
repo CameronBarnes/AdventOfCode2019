4370a41be2242bf9bf7361b205468b627a550bf5
Cameron Barnes <cameron_barnes@outlook.com>
2024-11-26 09:56:27 -0800

    day 6 part 2 working on test input, failing to parse challenge input correctly

--- day-6/src/part2.rs
+++ day-6/src/part2.rs
@@ -1,11 +1,13 @@
 use std::{cell::RefCell, rc::Rc};
 
 use ahash::{HashMap, HashMapExt};
+use itertools::Itertools;
 
 struct Object {
     name: String,
     parent: Option<Rc<RefCell<Object>>>,
     children: Vec<Rc<RefCell<Object>>>,
+    depth: usize,
 }
 
 impl Object {
@@ -14,6 +16,7 @@ impl Object {
             name: name.to_string(),
             parent,
             children: Vec::default(),
+            depth: 0,
         }
     }
 
@@ -37,9 +40,37 @@ impl Object {
             name: name.to_string(),
             parent,
             children: vec![child.clone()],
+            depth: 0,
         }));
         child.borrow_mut().parent = Some(this.clone());
         this
+    }
+
+    fn set_depth(&mut self, depth: usize) {
+        self.depth = depth;
+        self.print();
+        assert_eq!(self.depth == 0, self.parent.is_none());
+        for child in &self.children {
+            child.borrow_mut().set_depth(self.depth + 1);
+        }
+    }
+
+    fn get_parent(&self) -> Option<Rc<RefCell<Object>>> {
+        self.parent.clone()
+    }
+
+    fn print(&self) {
+        println!(
+            "Node: {}, Depth: {}, Parent: {:?}, Children: {:?}",
+            self.name,
+            self.depth,
+            self.parent.is_some(),
+            self.children
+                .iter()
+                .cloned()
+                .map(|child| child.borrow().name.clone())
+                .collect_vec()
+        );
     }
 }
 
@@ -69,8 +100,73 @@ fn load_map(input: &str) -> HashMap<String, Rc<RefCell<Object>>> {
 
 #[tracing::instrument]
 pub fn process(input: &str) -> String {
+    // Load the map from the input
     let map = load_map(input);
-    todo!()
+    // Fill out all the orbit depth values so that we can find an efficient path
+    map.get("COM")
+        .expect("COM must be present at the core of the map")
+        .borrow_mut()
+        .set_depth(0);
+    // Find the efficient path
+    let mut current_santa = map
+        .get("SAN")
+        .expect("Santa must be present on the map somewhere")
+        .clone();
+    let mut current_you = map
+        .get("YOU")
+        .expect("You must be present on the map somewhere")
+        .clone();
+    let mut steps = 0usize;
+    let mut cont = true;
+    while cont {
+        match current_santa
+            .clone()
+            .borrow()
+            .depth
+            .cmp(&current_you.clone().borrow().depth)
+        {
+            std::cmp::Ordering::Equal => {
+                // We're allowed to unwrap the parent values here because they cant be empty unless
+                // something has gone terribly wrong
+                if current_santa
+                    .borrow()
+                    .get_parent()
+                    .unwrap_or_else(|| {
+                        current_santa.borrow().print();
+                        panic!("No parent! from santa's side");
+                    })
+                    .borrow()
+                    .name
+                    == current_you
+                        .borrow()
+                        .get_parent()
+                        .unwrap_or_else(|| {
+                            current_you.borrow().print();
+                            panic!("No parent! from your side");
+                        })
+                        .borrow()
+                        .name
+                {
+                    cont = false;
+                } else {
+                    current_santa = current_santa.clone().borrow().get_parent().unwrap();
+                    current_you = current_you.clone().borrow().get_parent().unwrap();
+                    steps += 2;
+                }
+            }
+            std::cmp::Ordering::Less => {
+                // Santa is less, you need to go down
+                current_you = current_you.clone().borrow().get_parent().unwrap();
+                steps += 1;
+            }
+            std::cmp::Ordering::Greater => {
+                // You are less, santa needs to go down
+                current_santa = current_santa.clone().borrow().get_parent().unwrap();
+                steps += 1;
+            }
+        }
+    }
+    steps.to_string()
 }
 
 #[cfg(test)]
@@ -79,8 +175,33 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+L)YOU
+I)SAN";
+        assert_eq!("5", process(input));
+        let input = "COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN";
+        assert_eq!("4", process(input));
     }
 }
